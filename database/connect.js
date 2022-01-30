24224ff86f8ac2fe5445ebb8d18763e49f11da3d
manoram123 <61000567+manoram123@users.noreply.github.com>
2022-01-30 15:24:14 +0545

    database select

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://SPYDEi:Mano_ram123@cluster0.3oruv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://SPYDEi:Mano_ram123@cluster0.3oruv.mongodb.net/enomoChatApp?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
